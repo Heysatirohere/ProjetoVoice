f1aad856a374b81bf8dd6ac77395aad2883b441d
Heysatirohere <satirog719@gmail.com>
2024-09-06 19:27:34 -0300

    Uploding project

---
+++ app/src/main/java/com/example/voice/MainActivity.java
@@ -0,0 +1,56 @@
+package com.example.voice;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.splashscreen.SplashScreen;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        SplashScreen.installSplashScreen(this);
+
+        // Configura os insets para ajustar a padding da view principal
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        // Configura o BottomNavigationView
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(this::onNavigationItemSelected);
+    }
+
+    // Listener para o BottomNavigationView
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        int itemId = item.getItemId();
+
+        if (itemId == R.id.nav_home) {
+            startActivity(new Intent(MainActivity.this, MainActivity.class));
+            return true;
+        } else if (itemId == R.id.nav_warning) {
+            startActivity(new Intent(MainActivity.this, complaint.class));
+            return true;
+        } else if (itemId == R.id.nav_image) {
+            startActivity(new Intent(MainActivity.this, notice.class));
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
